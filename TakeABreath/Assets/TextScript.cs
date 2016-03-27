74a0ad92e35f0a2324f35dd7fb3a056e349acc86
PrLayton <gogokoko3@hotmail.fr>
2016-03-27 19:06:55 +0200

    Add Multiplayer Unity Project

---
+++ TakeABreath/Assets/TextScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TextScript : NetworkBehaviour {
+
+    GameObject canvas;
+    [SerializeField]
+    Text tx;
+
+    [SyncVar(hook = "OnStr")]
+    string str;
+    private bool editingMode;
+
+    [SerializeField]
+    GameObject selectionFeedback;
+
+    float pressTime;
+
+    // Use this for initialization
+    void Start () {
+        canvas = GameObject.Find("Canvas");
+        transform.SetParent(canvas.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isClient)
+            return;
+
+        if (editingMode)
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                if (tx.text.Length > 0)
+                    tx.text = tx.text.Remove(tx.text.Length - 1);
+            }
+            else
+            {
+                tx.text = tx.text.Insert(tx.text.Length, Input.inputString);
+            }
+            if (Input.GetKey(KeyCode.Backspace))
+            {
+                pressTime += Time.deltaTime;
+                if (pressTime > 0.5f)
+                {
+                    if (tx.text.Length > 0)
+                        tx.text = tx.text.Remove(tx.text.Length - 1);
+                }
+            }
+            else
+            {
+                pressTime = 0;
+            }
+                if (Input.GetKeyDown(KeyCode.Return))
+                CmdChangeText(tx.text.Insert(tx.text.Length, "\n"));
+            if(Input.GetKeyDown(KeyCode.Space))
+                CmdChangeText(tx.text);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Zone")
+        {
+            GetComponent<NetworkIdentity>().RemoveClientAuthority(GetComponent<NetworkIdentity>().clientAuthorityOwner);
+            GetComponent<NetworkIdentity>().AssignClientAuthority(other.GetComponentInParent<Move>().connectionToClient); //other.GetComponentInParent<Move>().connectionToClient
+            editingMode = true;
+            selectionFeedback.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Zone")
+        {
+            editingMode = false;
+            selectionFeedback.SetActive(false);
+        }
+    }
+
+    [Command]
+    void CmdChangeText(string txt)
+    {
+        str = txt;
+        tx.text = str;
+    }
+
+    void OnStr(string netwStr)
+    {
+        tx.text = netwStr;
+    }
+}
---
+++ TakeABreath/Assets/ToolsForPlayer.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ToolsForPlayer : NetworkBehaviour
+{
+    enum ToolMode { cube, text, image};
+
+    ToolMode tm;
+
+    [SerializeField]
+    GameObject prefab1;
+    [SerializeField]
+    GameObject prefab2;
+    [SerializeField]
+    GameObject prefab3;
+
+    Text tx;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!isLocalPlayer)
+            return;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            tm = ToolMode.cube;
+        }else
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            tm = ToolMode.text;
+        }else
+        if (Input.GetKeyDown(KeyCode.Alpha3)){
+            tm = ToolMode.image;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch (tm)
+            {
+                case ToolMode.cube:
+                    CmdSpawnCube();
+                    //(this.transform.forward * 10, ForceMode.Impulse);
+                    break;
+                case ToolMode.text:
+                    CmdSpawnText();
+                    break;
+                case ToolMode.image:
+                    CmdSpawnImage();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public static Texture2D LoadPNG(string filePath)
+    {
+        Texture2D tex = new Texture2D(2, 2, TextureFormat.BGRA32, false);
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //will auto-resize the texture dimensions.
+        }
+        return tex;
+    }
+
+    [Command]
+    void CmdSpawnCube()
+    {
+        NetworkServer.Spawn(Instantiate(prefab1, this.transform.position + this.transform.forward * 3, this.transform.rotation) as GameObject);
+    }
+
+    [Command]
+    void CmdSpawnText()
+    {
+        GameObject g = (Instantiate(prefab2, this.transform.position + this.transform.forward * 3, this.transform.rotation) as GameObject);
+        NetworkServer.SpawnWithClientAuthority(g, this.gameObject);
+    }
+
+    [Command]
+    void CmdSpawnImage()
+    {
+        GameObject g = (Instantiate(prefab3, this.transform.position + this.transform.forward * 3, this.transform.rotation) as GameObject);
+        NetworkServer.Spawn(g);
+    }
+}
