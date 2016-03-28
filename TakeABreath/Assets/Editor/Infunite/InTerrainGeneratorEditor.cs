855b3eaf653e3be26ebbc4cb7aeba6eb16bf7249
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-03-29 00:21:17 +0200

    Added Scripts For Terrain Generation

---
+++ TakeABreath/Assets/Editor/Infunite/InTerrainGeneratorEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(InTerrainGenerator))]
+public class InTerrainGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        InTerrainGenerator terrainGen = (InTerrainGenerator)target;
+
+        if(DrawDefaultInspector()) {
+            if(terrainGen.autoUpdate)
+            {
+                terrainGen.DrawTerrainInEditor();
+            }
+        }
+
+        if(GUILayout.Button("Generate Terrain"))
+        {
+            terrainGen.DrawTerrainInEditor();
+        }
+    }
+
+}
---
+++ TakeABreath/Assets/Scripts/InTerrainGenerator.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+
+public class InTerrainGenerator : MonoBehaviour {
+
+    public enum DrawMode
+    {
+        NoiseMap, 
+        ColorMap,
+        Mesh
+    }
+
+    public DrawMode drawMode;
+
+    public const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int editorLevelOfDetail;
+    public float noiseScale=1;
+
+    public int octaves=1;
+    [Range(0f, 1f)]
+    public float persistance=0;
+    public float lacunarity=0;
+
+    public int seed;
+    public Vector2 offset;
+    public float meshHeightMultiplier=1;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    Queue<TerrainThreadInfo<TerrainData>> terrainDataThreadInfoQueue = new Queue<TerrainThreadInfo<TerrainData>>();
+    Queue<TerrainThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<TerrainThreadInfo<MeshData>>();
+
+    public void DrawTerrainInEditor()
+    {
+        TerrainData terrainData = GenerateTerrainData();
+
+        InTerrainDisplay display = FindObjectOfType<InTerrainDisplay>();
+        if (drawMode == DrawMode.NoiseMap)
+            display.DrawTexture(InTextureGenerator.TextureFromHeightMap(terrainData.heightMap));
+        else if (drawMode == DrawMode.ColorMap)
+            display.DrawTexture(InTextureGenerator.TextureFromColorMap(terrainData.colorMap, mapChunkSize, mapChunkSize));
+        else if (drawMode == DrawMode.Mesh)
+            display.DrawMesh(InMeshGenerator.GenerateTerrainMesh(terrainData.heightMap, meshHeightMultiplier, meshHeightCurve, editorLevelOfDetail), InTextureGenerator.TextureFromColorMap(terrainData.colorMap, mapChunkSize, mapChunkSize));
+
+    }
+
+    public void RequestTerrainData(Action<TerrainData> callback)
+    {
+        ThreadStart threadStart = delegate
+        {
+            TerrainDataThread(callback);
+        };
+
+        new Thread(threadStart).Start();
+    }
+
+    void TerrainDataThread(Action<TerrainData> callback)
+    {
+        TerrainData terrainData = GenerateTerrainData();
+        lock (terrainDataThreadInfoQueue) //Pour attendre la fin de l'exécution 
+        {
+            terrainDataThreadInfoQueue.Enqueue(new TerrainThreadInfo<TerrainData>(callback, terrainData));
+        }
+        
+    }
+
+    public void RequestMeshData(TerrainData terrainData, int lod, Action<MeshData> callback)
+    {
+        ThreadStart threadStart = delegate
+        {
+            MeshDataThread(terrainData, lod, callback);
+        };
+
+        new Thread(threadStart).Start();
+    } 
+
+    void MeshDataThread(TerrainData terrainData, int lod, Action<MeshData> callback)
+    {
+        MeshData meshData = InMeshGenerator.GenerateTerrainMesh(terrainData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
+        lock(meshDataThreadInfoQueue)
+        {
+            meshDataThreadInfoQueue.Enqueue(new TerrainThreadInfo<MeshData>(callback, meshData));
+        }
+    }
+
+    void Update()
+    {
+        if(terrainDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < terrainDataThreadInfoQueue.Count;i++) {
+                TerrainThreadInfo<TerrainData> threadInfo = terrainDataThreadInfoQueue.Dequeue();
+                threadInfo.callback(threadInfo.parameter);
+            }
+        }
+
+        if(meshDataThreadInfoQueue.Count > 0)
+        {
+            for(int i=0;i<meshDataThreadInfoQueue.Count;i++)
+            {
+                TerrainThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
+                threadInfo.callback(threadInfo.parameter);
+            }
+        }
+    }
+
+    TerrainData GenerateTerrainData()
+    {
+        float[,] noiseMap = InNoise.GenerateNoiseMap(mapChunkSize, mapChunkSize,seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+
+        for(int y = 0; y < mapChunkSize; y++)
+        {
+            for(int x=0;x<mapChunkSize;x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i=0;i<regions.Length;i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new TerrainData(noiseMap, colorMap);
+
+    }
+
+    void OnValidate()
+    {
+        if (lacunarity < 1)
+            lacunarity = 1;
+        if (octaves < 0)
+            octaves = 0;
+    }
+
+    struct TerrainThreadInfo<T>
+    {
+        public readonly Action<T> callback;
+        public readonly T parameter;
+
+        public TerrainThreadInfo(Action<T> callback, T parameter)
+        {
+            this.callback = callback;
+            this.parameter = parameter;
+        }
+
+    }
+
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+
+}
+
+public struct TerrainData
+{
+    public readonly float[,] heightMap;
+    public readonly Color[] colorMap;
+
+    public TerrainData(float[,] heightMap, Color[] colorMap)
+    {
+        this.heightMap = heightMap;
+        this.colorMap = colorMap;
+    }
+}
