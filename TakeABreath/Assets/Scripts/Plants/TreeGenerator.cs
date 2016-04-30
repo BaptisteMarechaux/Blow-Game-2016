68d7d39057b660ca57c158ac2ec396716c3ccaba
charline_rochelle <charline.rochelle@gmail.com>
2016-04-30 15:55:10 +0200

    Plants
    Fun :D

--- TakeABreath/Assets/Scripts/Plants/TreeGenerator.cs
+++ TakeABreath/Assets/Scripts/Plants/TreeGenerator.cs
@@ -8,10 +8,10 @@ public class TreeGenerator : MonoBehaviour
 
     public void Start ( )
     {
-        float rad = 1f;
+        float rad = 2f;
         for ( int i = 0 ; i < _trees . Length ; ++i, ++rad )
         {
-            this . _trees [ i ] . Init ( new Vector3 ( Mathf . Cos ( i ) * rad , 0 , Mathf . Sin ( i ) * rad ) , .225f , .5f , float . MaxValue , 100 , new Vector3 ( 0 , 0 , 0 ) , 500 , true );
+            this . _trees [ i ] . Init ( new Vector3 ( Mathf . Cos ( i ) * rad , 0 , Mathf . Sin ( i ) * rad ) , .225f , .25f , float . MaxValue , 50 , new Vector3 ( 0 , 0 , 0 ) , 500 , true );
         }
     }
 
