9ee3d0249eff97bda522d03b9262cea5e571d504
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-05-10 00:06:16 +0200

    New Tutorial Scene started

---
+++ TakeABreath/Assets/Scripts/Character/OfflineCharacterController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OfflineCharacterController : MonoBehaviour {
+    [SerializeField]
+    float speed = 5;
+    [SerializeField]
+    float rotationSpeed = 10;
+    float h, v;
+    Vector3 transformPositionVector = new Vector3();
+
+    [SerializeField]
+    Camera mainCamera;
+    [SerializeField]
+    float cameraDistance=10;
+    [SerializeField]
+    float cameraHeight=5;
+    [SerializeField]
+    float cameraSpeed=10;
+    [SerializeField]
+    float cameraRotationSpeed=10;
+
+    //[SerializeField]
+    //float cameraSpeed = 1.0f;
+
+    Vector3 forward;
+    Vector3 right;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        forward = mainCamera.transform.forward;
+        right = new Vector3(forward.z, 0, -forward.x);
+
+        transformPositionVector = (h * right + v * forward) * speed * Time.deltaTime;
+        transformPositionVector.y = 0;
+        transform.position = Vector3.MoveTowards(transform.position, transformPositionVector + transform.position, Time.deltaTime * speed);
+
+        if (h != 0 || v != 0)
+        {
+            GetRotation(new Vector3(h, 0, v));
+        }
+
+
+        //mainCamera.transform.LookAt(transform);
+        var lookRotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+        mainCamera.transform.rotation = Quaternion.Slerp(mainCamera.transform.rotation, lookRotation, cameraRotationSpeed * Time.deltaTime);
+
+       
+       
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(transform.position.x, transform.position.y + cameraHeight, transform.position.z - cameraDistance), cameraSpeed * Time.deltaTime);
+
+        
+    }
+
+    void GetRotation(Vector3 toRotation)
+    {
+        Vector3 relativePos = mainCamera.transform.TransformDirection(toRotation);
+        relativePos.y = 0.0f;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ TakeABreath/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum TutorialState
+{
+    introduction,
+    explainations
+}
+
+public class TutorialManager : MonoBehaviour {
+    [SerializeField]
+    Text centralText;
+
+    [SerializeField]
+    Image blackBackgroundImage;
+
+    //Liste de ce qui va être dit pendant la petite introduction
+    [SerializeField]
+    string[] introductionTextList;
+
+    TutorialState state;
+
+    public int readIndex = 0;
+	// Use this for initialization
+	void Start () {
+        if(introductionTextList.Length>0)
+        {
+            centralText.text = "";
+            StartCoroutine("DisplayText", introductionTextList[0]);
+        }
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ReadNextText()
+    {
+        if(state == TutorialState.introduction)
+        {
+            if(readIndex +1 < introductionTextList.Length)
+            {
+                readIndex += 1;
+                StopCoroutine("DisplayText");
+                StartCoroutine("DisplayText", introductionTextList[readIndex]);    
+            }
+        }
+    }
+
+    IEnumerator DisplayText(string readString)
+    {
+        centralText.text = "";
+        for (int i = 0; i < readString.Length; i++)
+        {
+            centralText.text += readString[i];
+            yield return new WaitForSeconds(0.06f);
+        }
+    }
+}
