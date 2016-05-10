525b361334bf412953419c68e42d868b1d63f03b
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-05-10 16:44:54 +0200

    Scripted Event manager created

---
+++ TakeABreath/Assets/Editor/ScriptedEventManagerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ScriptedEventManager))]
+public class ScriptedEventManagerEditor : Editor {
+
+	override public void OnInspectorGUI()
+    {
+        var scriptedEventManager = target as ScriptedEventManager;
+
+        scriptedEventManager.activated = GUILayout.Toggle(scriptedEventManager.activated, "Activate");
+        if(scriptedEventManager.activated)
+        {
+            scriptedEventManager.theString = EditorGUILayout.TextArea(scriptedEventManager.theString);
+            scriptedEventManager.i = EditorGUILayout.IntSlider(scriptedEventManager.i, 0, 100);
+        }
+    }
+}
---
+++ TakeABreath/Assets/Scripts/ScriptedEventManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct EventCommand
+{
+    EventCommandObjectType objectType;
+    EventCommandActionType action;
+
+    public void ExecuteCommand()
+    {
+
+    }
+}
+
+[System.Serializable]
+public struct EventCommandObject
+{
+
+}
+
+public enum EventCommandObjectType{
+    gameObject,
+    camera,
+    text
+
+}
+
+public enum EventCommandActionType
+{
+    translate,
+    rotate,
+    scale,
+    write
+
+}
+
+public class ScriptedEventManager : MonoBehaviour {
+    public EventCommand[] events;
+
+    public bool activated;
+    public string theString = "Some text";
+    public int i;
+	// Use this for initialization
+	void Start () {
+	}
+	
+}
