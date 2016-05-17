f7719d3eecb516d0feb4b908b201f4a18badb8b7
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-05-17 16:59:06 +0200

    changde scripted event structure

--- TakeABreath/Assets/Scripts/ScriptedEventManager.cs
+++ TakeABreath/Assets/Scripts/ScriptedEventManager.cs
@@ -2,24 +2,6 @@
 using System.Collections;
 using UnityEngine.UI;
 
-[System.Serializable]
-public struct EventCommand
-{
-    EventCommandObjectType objectType;
-    EventCommandActionType action;
-
-    public void ExecuteCommand()
-    {
-
-    }
-}
-
-[System.Serializable]
-public struct EventCommandObject
-{
-
-}
-
 public enum EventCommandObjectType{
     gameObject,
     camera,
@@ -36,14 +18,36 @@ public enum EventCommandActionType
 
 }
 
+public enum ConditionType
+{
+    position,
+
+}
+
 public class ScriptedEventManager : MonoBehaviour {
-    public EventCommand[] events;
+    public ScriptedEvent[] events;
 
     public bool activated;
     public string theString = "Some text";
     public int i;
 	// Use this for initialization
 	void Start () {
+
 	}
 	
+}
+
+public class ScriptedEvent : MonoBehaviour
+{
+    ConditionType[] conditionTriggers;
+
+
+    bool VerifyCondition()
+    {
+        var validated = false;
+        for(int i=0;i<conditionTriggers.Length;i++)
+        {
+            if()
+        }
+    }
 }
