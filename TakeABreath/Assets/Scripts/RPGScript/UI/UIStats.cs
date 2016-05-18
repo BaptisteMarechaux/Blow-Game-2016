84fa8780316124ea6d8b864523c7e44ed2191891
Pandaka <tristancailleau@hotmail.fr>
2016-05-18 11:24:42 +0200

    ADD : - Pannel for stats - Button for display/hide this pannel - New prefab of apocalyptical doom player solo !!!

---
+++ TakeABreath/Assets/Scripts/RPGScript/UI/UIStats.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIStats : MonoBehaviour {
+
+    [SerializeField]
+    PlayerPrefManager ppm;
+    [SerializeField]
+    GameObject panel;
+    [SerializeField]
+    Text nom;
+    [SerializeField]
+    Text niveau;
+    [SerializeField]
+    Text vie;
+    [SerializeField]
+    Text force;
+    [SerializeField]
+    Text constitution;
+    [SerializeField]
+    Text intelligence;
+    [SerializeField]
+    Text volonte;
+
+    // Use this for initialization
+    void Start () {
+        UpdateValue();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Display()
+    {
+        if(panel.activeSelf)
+            panel.SetActive(false);
+        else
+            panel.SetActive(true);
+        UpdateValue();
+    }
+
+    public void UpdateValue()
+    {
+        nom.text = ppm.PlayerName();
+        vie.text = ppm.GetValue("VieMax").ToString();
+        force.text = ppm.GetValue("Force").ToString();
+        constitution.text = ppm.GetValue("Constitution").ToString();
+        intelligence.text = ppm.GetValue("Intelligence").ToString();
+        volonte.text = ppm.GetValue("Volonte").ToString();
+    }
+}
