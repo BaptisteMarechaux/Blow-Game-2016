576ece679912f009e9003b6ffd63d357f6b2916d
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-06-15 09:54:35 +0200

    Base for spells

---
+++ TakeABreath/Assets/Scripts/RPGScript/Other/Spell.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spell : MonoBehaviour {
+
+    [SerializeField]
+    protected string spellName;
+    [SerializeField]
+    protected int power;
+    [SerializeField]
+    protected float cooldown; //value in seconds
+
+
+}
