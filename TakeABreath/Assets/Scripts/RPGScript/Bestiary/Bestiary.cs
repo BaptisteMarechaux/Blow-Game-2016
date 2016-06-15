d4752318053f465427dcae92e33403681ecdaf4a
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-06-15 15:33:13 +0200

    UI for bestiary

---
+++ TakeABreath/Assets/Scripts/RPGScript/Bestiary/Bestiary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bestiary : MonoBehaviour {
+    BestiaryMonster[] monsters;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnable()
+    {
+
+    }
+}
+
+[System.Serializable]
+public struct BestiaryMonster
+{
+    string name;
+    bool discovered;
+    string[] spells;
+}
--- TakeABreath/Assets/Scripts/RPGScript/Other/Spell.cs
+++ TakeABreath/Assets/Scripts/RPGScript/Other/Spell.cs
@@ -6,9 +6,12 @@ public class Spell : MonoBehaviour {
     [SerializeField]
     protected string spellName;
     [SerializeField]
+    protected string spellDescription;
+    [SerializeField]
     protected int power;
     [SerializeField]
     protected float cooldown; //value in seconds
 
-
+    [SerializeField]
+    protected GameObject owner;
 }
